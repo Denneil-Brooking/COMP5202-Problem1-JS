d1ce269efd301425db11917a898ede6a697d2b4d
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2017-09-04 11:37:48 +1200

    JS T1 done

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-//
-let 
-//
-//
-//
+//program function
+alert("This program takes two numbers & finds the difference between them");
+//Ask user for numbers input
+let num1 = parseInt = prompt("Please enter your first number", 00);
+console.log("The 1st number you chose was " + num1);
+alert("The 1st number you chose was " + num1);
+let num2 = parseInt = prompt("Please enter your second number", 00);
+console.log("The 2nd number you chose was " + num2);
+alert("The 2nd number you chose was " + num2);
+//if statement for the outcomes.
+if (num1 < num2)
+    {
+        console.log(num2 + " is greater then " + num1);
+        alert(num2 + " is greater then " + num1);
+    } else 
+    {
+        console.log(num1 + " is greater then " + num2);
+        alert(num1 + " is greater then " + num2);
+    }
+//program end
